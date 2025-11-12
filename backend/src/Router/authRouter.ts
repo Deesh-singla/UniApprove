4490d0d7c5c15c1d62dbcd222edcae29803ffeec
Deesh <145862991+Deesh-singla@users.noreply.github.com>
2025-11-12 22:11:58 +0530

    Delete backend/src directory

--- backend/src/Router/authRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import login from "../controller/loginRouter.js";
-
-const authRouter=express.Router();
-
-authRouter.post("/login",login)
-
-export default authRouter;
